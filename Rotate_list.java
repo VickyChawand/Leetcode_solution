4322f90764174f1a2c0050404971a055c8e3ce26
Vicky Chawand <shivanshchawand044@gmail.com>
2023-11-29 15:08:08 +0530

    Update Rotate_list.java

--- Rotate_list.java
+++ Rotate_list.java
@@ -7,16 +7,18 @@ The basic idea of the solution that I am about to implement is quite simple but 
 1) Add Bases
 First, we check lf the linked list that we are given is empty or if k is 0. If one or more conditions are met, we can just return the head since there are noting that we need to do
 
-    if not head or k == 0:
-        return head
+    if (head == null || k == 0) {
+            return head;
+        }
 2) Find the Length of the Given Linked List
 In order to know where we want to break the linked list, we first need to know how long the linked list is. We can find that by simply implementing a basic while loop that traverse the linked list and adding one in each iteration to a variable that I initialized named length
 
-    current = head
-    length = 0
-    while current:
-        length+=1
-        current = current.next
+     ListNode current = head;
+        int length = 0;
+        while (current != null) {
+            length++;
+            current = current.next;
+        }
 3) The Special k
 The key part of the algorithm is what we do with k:
 
@@ -41,34 +43,37 @@ i.e) 10%3 = 1
 
 The point of doing this is that we don't need to rotate a linked list k times If k is divisible by length. If k is not divisible by length, we only need to rotate the linked list the remainder of times that we calculated using the mod operator. Take the example of a-->b-->c, k=4. We don't actually need to rotate the linked list four times because doing the first three times will just reusult in a linked list that we stared off with. Therefore we really only need to rotate it one time, which is going to make a difference.
 
-        if k == length:
-            return head
-        k =  k % length
-        if k == 0:
-            return head
+        if (k == length) {
+            return head;
+        }
+        k = k % length;
+        if (k == 0) {
+            return head;
+        }
 5) The Number of Times to Move Forward
 This part is fairly straitht forward. But I want to point out the reason why we subtract 1 from go. We want to stop one position before where we need to break our list because I want to properly end the first part of the list by pointing its last node to none. Before that though, I am going to assign a pointer(nextFirst) to the first node of the seocnd linked list so I don't lose them.
 
 There is an edge case that I encountered where the length of the linked list would be 1 and we can't rotate it with anything if there is only one node in the linked list so we will just return head
 
-        go = length - k
-        first = head
-
-        for i in range(go-1):
-            first = first.next
-
-        nextFirst = first.next
-        first.next = None
-        if length == 1:
-            return head
+        int go = length - k;
+        ListNode first = head;
+        for (int i = 0; i < go - 1; i++) {
+            first = first.next;
+        }
+        ListNode nextFirst = first.next;
+        first.next = null;
+        if (length == 1) {
+            return head;
+        }
 5) Connect Two Parts
 Now, we will iterate through the second part of the linked list and when it reaches the last node, we will point that to the first node of the first part of the linked list (head). Lastly, we will return nextFirst which is the new begining of our linked list.
 
-        tail = nextFirst
-        while tail.next:
-            tail = tail.next
-        tail.next = head
-        return nextFirst
+        ListNode tail = nextFirst;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = head;
+        return nextFirst;
 Complexity
 Time complexity:
 We are iterating through the linked list one time in the beginning of our algorithm so that would give us O(n) time complexity.
